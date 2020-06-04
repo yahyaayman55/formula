e18ca08f5573bd0534f91ff389d9cee4efd87d75
Laimonas Turauskas <laimiux@gmail.com>
2020-06-04 16:42:01 -0700

    Setup example with compose.

---
+++ samples/stopwatch-compose/src/main/java/com/instacart/formula/stopwatch/StopwatchRenderView.kt
@@ -0,0 +1,27 @@
+package com.instacart.formula.stopwatch
+
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import com.instacart.formula.Renderer
+import com.instacart.formula.RenderView
+
+class StopwatchRenderView(root: ViewGroup): RenderView<StopwatchRenderModel> {
+    private val timePassed: TextView = root.findViewById(R.id.time_passed_text_view)
+    private val startStopButton: Button = root.findViewById(R.id.start_stop_button)
+    private val resetButton: Button = root.findViewById(R.id.reset_button)
+
+    override val render: Renderer<StopwatchRenderModel> = Renderer { model ->
+        timePassed.text = model.timePassed
+
+        startStopButton.text = model.startStopButton.text
+        startStopButton.setOnClickListener {
+            model.startStopButton.onSelected()
+        }
+
+        resetButton.text = model.resetButton.text
+        resetButton.setOnClickListener {
+            model.resetButton.onSelected()
+        }
+    }
+}
